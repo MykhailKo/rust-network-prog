79567b662729f209b3d4ec3df4df969c0df9ea75
MykhailKo <tuareg1812@gmail.com>
2023-02-19 15:05:36 +0200

    feat: processing opts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use getopt::Opt;
+
 fn main() {
-    println!("Hello, world!");
+    // args = ["program", "-abc", "-d", "foo", "-e", "bar"];
+    let mut args: Vec<String> = std::env::args().collect();
+    let mut opts = getopt::Parser::new(&args, "nodename:port:");
+
+    let addressSeparator: String = ":".to_owned();
+    let mut nodename = String::new();
+    let mut port = String::new();
+
+    loop {
+        match opts.next().transpose()? {
+            None => break,
+            Some(opt) => match opt {
+                Opt("nodename", Some(arg)) => nodename = arg.clone(),
+                Opt("port", Some(arg)) => nodename = arg.clone(),
+                _ => unreachable!(),
+            },
+        }
+    }
+
+    let textSockAddr: String = (nodename + &addressSeparator) + &port;
+
+    println!("Translating address: {textSockAddr}")
+
 }
