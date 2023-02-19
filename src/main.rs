51d0d5dd079e32a8fd0f5ce42a9b39f50bc9d050
MykhailKo <tuareg1812@gmail.com>
2023-02-19 15:14:12 +0200

    chore: fix type mismatches

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ use getopt::Opt;
 fn main() {
     // args = ["program", "-abc", "-d", "foo", "-e", "bar"];
     let mut args: Vec<String> = std::env::args().collect();
-    let mut opts = getopt::Parser::new(&args, "nodename:port:");
+    let mut opts = getopt::Parser::new(&args, "n:p:");
 
     let addressSeparator: String = ":".to_owned();
     let mut nodename = String::new();
     let mut port = String::new();
 
     loop {
-        match opts.next().transpose()? {
+        match opts.next().transpose() {
             None => break,
             Some(opt) => match opt {
-                Opt("nodename", Some(arg)) => nodename = arg.clone(),
-                Opt("port", Some(arg)) => nodename = arg.clone(),
+                Opt('n', Some(arg)) => nodename = arg.clone(),
+                Opt('p', Some(arg)) => port = arg.clone(),
                 _ => unreachable!(),
             },
         }
